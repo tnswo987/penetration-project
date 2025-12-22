78cecf326a15e417ab285d981170e3c1d1eec6d2
KSJ <yaja7698@naver.com>
2025-12-23 01:59:44 +0900

    [pdm update]

--- PdM/mpu6050_training.py
+++ PdM/mpu6050_training.py
@@ -2,8 +2,10 @@ import numpy as np
 import pandas as pd
 from keras.models import Sequential
 from keras.layers import LSTM, RepeatVector, TimeDistributed, Dense
+from keras.callbacks import EarlyStopping
 from sklearn.preprocessing import StandardScaler
 from matplotlib import pyplot as plt
+import joblib
 
 dataframe = pd.read_csv("mpu6050_train.csv")
 
@@ -71,23 +73,62 @@ model.add(TimeDistributed(Dense(len(FEATURE_COLS))))
 
 model.compile(optimizer='adam', loss='mae')
 model.summary()
+early_stop = EarlyStopping(
+    monitor='val_loss',
+    patience=8,
+    restore_best_weights=True,
+)
 
 history = model.fit(
     trainX,
     trainX,
-    epochs=30,
+    epochs=100,
     batch_size=32,
     validation_split=0.1,
     shuffle=False,
+    callbacks=[early_stop],
     verbose=1
 )
 
-model.save("lstm_ae_mpu6050.h5")
+model.save("LSTM_AutoEncoder_MPU6050.h5")
 print("모델 저장 완료")
 
+joblib.dump(scaler, "mpu6050_scaler.pkl")
+print("스케일러 저장 완료")
+
+# loss, val_loss 그래프 시각화
 plt.figure(figsize=(10, 4))
 plt.plot(history.history['loss'], label='Train Loss')
 plt.plot(history.history['val_loss'], label='Val Loss')
 plt.legend()
+plt.grid()
+plt.show()
+
+# 학습/검증셋 reconstruction error 비교
+train_recon = model.predict(trainX)
+train_error = np.mean(np.abs(train_recon - trainX), axis=(1, 2))
+
+test_recon = model.predict(testX)
+test_error = np.mean(np.abs(test_recon - testX), axis=(1,2))
+
+threshold = np.percentile(train_error, 95)
+np.save("mpu6050_threshold.npy", threshold)
+print("Threshold 저장 완료:", threshold)
+
+plt.figure(figsize=(7,4))
+plt.hist(train_error, bins=50, alpha=0.6, label='Train')
+plt.hist(test_error, bins=50, alpha=0.6, label='Hold-out Normal')
+plt.legend()
+plt.title("Reconstruction Error Distribution (Normal Data)")
+plt.xlabel("MAE")
+plt.ylabel("Count")
+plt.grid()
+plt.show()
+
+plt.figure(figsize=(12,4))
+plt.plot(test_error)
+plt.title("Reconstruction Error Over Time (Hold-out Normal)")
+plt.xlabel("Sequence Index")
+plt.ylabel("MAE")
 plt.grid()
 plt.show()
